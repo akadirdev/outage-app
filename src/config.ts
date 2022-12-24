9124564ef60c429813b67e6bd311cfdf2638ce48
akadirdev <akadir.dd@gmail.com>
2022-12-25 00:44:53 +0300

    set config defaults

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const SITE_ID = process.env.SITE_ID;
-export const BASE_URL = process.env.BASE_URL;
-export const API_KEY = process.env.API_KEY;
+export const SITE_ID = process.env.SITE_ID ?? "norwich-pear-tree";
+export const BASE_URL =
+  process.env.BASE_URL ??
+  "https://api.krakenflex.systems/interview-tests-mock-api/v1/";
+export const API_KEY = process.env.API_KEY ?? "empty-api-key";
